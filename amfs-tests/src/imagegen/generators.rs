bb6d5577631dbca7859d52622763faf3b08d979b
chordtoll <git@chordtoll.com>
2022-07-25 20:11:42 -0700

    Add allocator tests

--- amfs-tests/src/imagegen/generators.rs
+++ amfs-tests/src/imagegen/generators.rs
@@ -1,9 +1,10 @@
 use std::fs::File;
 
 use amfs::{
-    AMPointerGlobal, AMPointerLocal, DiskGroup, FSGroup, Geometry, Superblock, BLOCK_SIZE,
-    SIGNATURE,
+    AMPointerGlobal, AMPointerLocal, AllocListEntry, DiskGroup, FSGroup, Geometry,
+    LinkedListGlobal, Superblock, BLOCK_SIZE, SIGNATURE,
 };
+use crc32fast::Hasher;
 
 /// Zero-filled file
 pub fn generate_0000(f: &File) {
@@ -138,3 +139,145 @@ pub fn generate_0006(f: &File) {
         d.write_at(i.loc(), &sb).unwrap();
     }
 }
+
+/// Build empty allocator list
+pub fn generate_0007(f: &File) {
+    generate_0006(f);
+
+    let mut d = super::utils::get_disk(f);
+    let locs = d.get_header_locs().unwrap();
+
+    let mut geo = Geometry::new();
+    geo.device_ids[0] = 0x0807060504030201;
+    let dg = DiskGroup::from_geo(geo, &[0x0807060504030201], &[d.clone()]).unwrap();
+
+    let mut group = AMPointerGlobal::new(3, 1, 0, 0);
+    group.update(&[Some(dg.clone())]).unwrap();
+
+    let mut alloc_list = AMPointerGlobal::new(4, 1, 0, 0);
+    alloc_list.update(&[Some(dg.clone())]).unwrap();
+
+    let mut res = [0u8; BLOCK_SIZE];
+    d.read_at(group.loc(), &mut res).unwrap();
+    res[0..16].clone_from_slice(&alloc_list.as_bytes());
+    d.write_at(group.loc(), &res).unwrap();
+
+    group.update(&[Some(dg)]).unwrap();
+
+    for i in locs {
+        let mut res = [0u8; BLOCK_SIZE];
+        d.read_at(i.loc(), &mut res).unwrap();
+        res[2048..2064].clone_from_slice(&group.as_bytes());
+        d.write_at(i.loc(), &res).unwrap();
+
+        let mut sb: Superblock = Superblock::new(0);
+        d.read_at(i.loc(), &mut sb).unwrap();
+        sb.update_checksum();
+        d.write_at(i.loc(), &sb).unwrap();
+    }
+}
+
+/// Build empty allocator
+pub fn generate_0008(f: &File) {
+    generate_0007(f);
+
+    let mut d = super::utils::get_disk(f);
+    let locs = d.get_header_locs().unwrap();
+
+    let mut geo = Geometry::new();
+    geo.device_ids[0] = 0x0807060504030201;
+    let dg = DiskGroup::from_geo(geo, &[0x0807060504030201], &[d.clone()]).unwrap();
+
+    let mut group = AMPointerGlobal::new(3, 1, 0, 0);
+    group.update(&[Some(dg.clone())]).unwrap();
+
+    let mut alloc_list = AMPointerGlobal::new(4, 1, 0, 0);
+
+    let mut alloc = AMPointerGlobal::new(5, 1, 0, 0);
+    alloc.update(&[Some(dg.clone())]).unwrap();
+
+    LinkedListGlobal::write_preallocd(
+        &Vec::from([AllocListEntry::new(0x0807060504030201, alloc)]),
+        &[Some(dg.clone())],
+        &[alloc_list],
+    )
+    .unwrap();
+
+    alloc_list.update(&[Some(dg.clone())]).unwrap();
+
+    let mut res = [0u8; BLOCK_SIZE];
+    d.read_at(group.loc(), &mut res).unwrap();
+    res[0..16].clone_from_slice(&alloc_list.as_bytes());
+    d.write_at(group.loc(), &res).unwrap();
+
+    group.update(&[Some(dg)]).unwrap();
+
+    for i in locs {
+        let mut res = [0u8; BLOCK_SIZE];
+        d.read_at(i.loc(), &mut res).unwrap();
+        res[2048..2064].clone_from_slice(&group.as_bytes());
+        d.write_at(i.loc(), &res).unwrap();
+
+        let mut sb: Superblock = Superblock::new(0);
+        d.read_at(i.loc(), &mut sb).unwrap();
+        sb.update_checksum();
+        d.write_at(i.loc(), &sb).unwrap();
+    }
+}
+
+/// Populate allocator
+pub fn generate_0009(f: &File) {
+    generate_0008(f);
+
+    let mut d = super::utils::get_disk(f);
+    let locs = d.get_header_locs().unwrap();
+
+    let mut geo = Geometry::new();
+    geo.device_ids[0] = 0x0807060504030201;
+    let dg = DiskGroup::from_geo(geo, &[0x0807060504030201], &[d.clone()]).unwrap();
+
+    let mut group = AMPointerGlobal::new(3, 1, 0, 0);
+    group.update(&[Some(dg.clone())]).unwrap();
+
+    let mut alloc_list = AMPointerGlobal::new(4, 1, 0, 0);
+
+    let mut alloc = AMPointerGlobal::new(5, 1, 0, 0);
+
+    LinkedListGlobal::write_preallocd(
+        &Vec::<u64>::from([0x40, 0x8000000000000006, 0x38, 0x8000000000000002]),
+        &[Some(dg.clone())],
+        &[alloc],
+    )
+    .unwrap();
+
+    alloc.update(&[Some(dg.clone())]).unwrap();
+
+    LinkedListGlobal::write_preallocd(
+        &Vec::from([AllocListEntry::new(0x0807060504030201, alloc)]),
+        &[Some(dg.clone())],
+        &[alloc_list],
+    )
+    .unwrap();
+
+    alloc_list.update(&[Some(dg.clone())]).unwrap();
+
+    let mut res = [0u8; BLOCK_SIZE];
+    d.read_at(group.loc(), &mut res).unwrap();
+    res[0..16].clone_from_slice(&alloc_list.as_bytes());
+    d.write_at(group.loc(), &res).unwrap();
+
+    group.update(&[Some(dg)]).unwrap();
+
+    for i in locs {
+        let mut res = [0u8; BLOCK_SIZE];
+        d.read_at(i.loc(), &mut res).unwrap();
+        res[2048..2064].clone_from_slice(&group.as_bytes());
+        d.write_at(i.loc(), &res).unwrap();
+
+        let mut sb: Superblock = Superblock::new(0);
+        d.read_at(i.loc(), &mut sb).unwrap();
+        sb.update_checksum();
+        d.write_at(i.loc(), &sb).unwrap();
+    }
+}
+
--- amfs-tests/tests/test_allocator.rs
+++ amfs-tests/tests/test_allocator.rs
@@ -1,6 +1,7 @@
 use amfs::DiskFile;
 use amfs_macros::*;
 use amfs_tests::{imagegen::generators::*, test_dump};
+use amos_std::error::AMErrorFS;
 
 #[test_fs]
 fn test_missing_allocator() {
@@ -15,5 +16,62 @@ fn test_missing_allocator() {
     let sb_locs = d.get_header_locs().unwrap();
     let sb = Superblock::read(d, sb_locs[0]).unwrap();
     let rg = sb.get_group(&[Some(dg.clone())]).unwrap();
+    rg.get_allocators(&[Some(dg)]).unwrap();
+}
+
+#[test_fs]
+fn test_empty_allocatorlist() {
+    use amfs::{test, Superblock};
+
+    generate_image!(7);
+
+    let d = load_image!(7);
+
+    let dg = test::dg::load_dg_disk_single(d.clone());
+
+    let sb_locs = d.get_header_locs().unwrap();
+    let sb = Superblock::read(d, sb_locs[0]).unwrap();
+    let rg = sb.get_group(&[Some(dg.clone())]).unwrap();
+    rg.get_allocators(&[Some(dg)]).unwrap();
+}
+
+#[test_fs]
+fn test_empty_allocator() {
+    use amfs::{test, Superblock};
+
+    generate_image!(8);
+
+    let d = load_image!(8);
+
+    let dg = test::dg::load_dg_disk_single(d.clone());
+
+    let sb_locs = d.get_header_locs().unwrap();
+    let sb = Superblock::read(d, sb_locs[0]).unwrap();
+    let rg = sb.get_group(&[Some(dg.clone())]).unwrap();
+
+    assert_eq!(
+        rg.get_allocators(&[Some(dg)])
+            .err()
+            .unwrap()
+            .downcast::<AMErrorFS>()
+            .unwrap(),
+        AMErrorFS::NoAllocator
+    );
+}
+
+#[test_fs]
+fn test_allocator() {
+    use amfs::{test, Superblock};
+
+    generate_image!(9);
+
+    let d = load_image!(9);
+
+    let dg = test::dg::load_dg_disk_single(d.clone());
+
+    let sb_locs = d.get_header_locs().unwrap();
+    let sb = Superblock::read(d, sb_locs[0]).unwrap();
+    let rg = sb.get_group(&[Some(dg.clone())]).unwrap();
+
     rg.get_allocators(&[Some(dg)]).unwrap();
 }
